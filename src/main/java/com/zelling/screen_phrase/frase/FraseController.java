867e83c9ad69f168662343e575d85e690085d035
FrancyelZelling <zellingf@gmail.com>
2024-12-30 14:42:18 -0300

    create endpoint to send phrase

--- src/main/java/com/zelling/screen_phrase/frase/FraseController.java
+++ src/main/java/com/zelling/screen_phrase/frase/FraseController.java
@@ -1,4 +1,20 @@
 package com.zelling.screen_phrase.frase;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/series/frases")
 public class FraseController {
+    @Autowired
+    private FraseService service;
+
+    @GetMapping
+    @ResponseBody
+    public Frase retornarFrase(){
+        return service.retornarFrase();
+    }
 }
--- src/main/java/com/zelling/screen_phrase/frase/FraseService.java
+++ src/main/java/com/zelling/screen_phrase/frase/FraseService.java
@@ -1,4 +1,11 @@
 package com.zelling.screen_phrase.frase;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class FraseService {
+    public Frase retornarFrase(){
+        Frase frase = new Frase("rocky", "teste", "rocky asap", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.9BPDlvHHp_8vhgkcxa9SrQAAAA%26pid%3DApi&f=1&ipt=8634639f41c2fc7409bd8c1e414170c434925b78f5bd322fd375e2a2e05c120d&ipo=images");
+        return frase;
+    }
 }
